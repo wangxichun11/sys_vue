20ad355a59d3f20054b5abd90ac1b0ed0df22f27
wangxichun11 <1506663762@qq.com>
2020-03-04 13:24:25 +0800

    添加搜索

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <el-menu-item index="/">Home</el-menu-item>
-      <el-menu-item index="/about">About</el-menu-item>
-      <el-menu-item index="/audit">audit</el-menu-item>
-    </el-menu>
-    <div class="line"></div>
-    <router-view />
+    <el-container>
+      <el-header>
+        <p>xxx管理系统</p>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-row class="tac">
+            <el-col :span="24">
+              <h1 style="text-align: center;">菜单</h1>
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+              >
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1"><router-link to="/">首页</router-link></el-menu-item>
+                    <el-menu-item index="1-1"><router-link to="/about">关于</router-link></el-menu-item>
+                    <el-menu-item index="1-1"><router-link to="/audit">审计</router-link></el-menu-item>
+                    
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
@@ -22,9 +61,11 @@ export default {
     };
   },
   methods: {
-    handleSelect(key, keyPath) {
+    handleOpen(key, keyPath) {
       console.log(key, keyPath);
-      this.$router.push({path:keyPath[0]});
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 };
@@ -34,21 +75,41 @@ export default {
   margin: 0;
   padding: 0;
 }
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100%;
+  
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.el-container {
+  height: 100%;
+}
+.el-header{
+  border-bottom: 1px solid #000;
+}
+.el-aside {
+  color: #333;
+  height: 100%;
+  border-right: 1px solid #000;
+}
+
+.el-main {
+  // background-color: #e9eef3;
+  color: #333;
+  height: 100%;
+  padding: 0;
+}
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
 }
 </style>
--- src/views/Audit.vue
+++ src/views/Audit.vue
@@ -1,13 +1,108 @@
 <template>
-    <div>
-        <h1>审计日志页面</h1>
+  <div>
+    <div class="audit_search">
+      <span>姓名：</span>
+      <el-input v-model="input_name" placeholder="请输入内容" clearable></el-input>
+      <span>地址：</span>
+      <el-input v-model="input_address" placeholder="请输入内容" clearable></el-input>
+      <span>日期：</span>
+      <el-date-picker v-model="input_data" type="date" placeholder="选择日期"></el-date-picker>
+      <span class="span_button">
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+      </span>
     </div>
+    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <style lang="less" scoped>
-    
+.audit_search {
+  span {
+    display: inline-block;
+    padding: 0 10px;
+  }
+  .span_button {
+    float: right;
+  }
+}
+.el-input {
+  width: 200px;
+}
 </style>
 <script>
 export default {
-    name:'audit'
-}
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex === 1) {
+        return "warning-row";
+      } else if (rowIndex === 3) {
+        return "success-row";
+      }
+      return "";
+    },
+    handleClick(row) {
+      console.log(row.date);
+      this.$confirm("你点击了查看！！！", "提示", {
+        dangerouslyUseHTMLString: true,
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "取消"
+          });
+        });
+    }
+  },
+  data() {
+    return {
+      input_data: "",
+      input_name: "",
+      input_address: "",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: "1"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: "2"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: "3"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          id: "4"
+        }
+      ]
+    };
+  }
+};
 </script>
